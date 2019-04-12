bcac4582dd9f5468d59b7d14a3cd4b69220112c9
jade243 <Aurelia.Autem@etu.unige.ch>
2019-04-12 15:52:04 +0200

    Added junit for the backend

---
+++ classads/src/test/java/ch/unige/pinfo/classads/service/ClassAdServiceImplTest.java
@@ -0,0 +1,34 @@
+package ch.unige.pinfo.classads.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClassAdServiceImplTest {
+
+	@Test
+	public void testCreateClassAd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetByTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteClassAd() {
+		fail("Not yet implemented");
+	}
+
+}
