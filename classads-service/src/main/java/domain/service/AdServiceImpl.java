ef9a54692a5d0761e75f8bb6874f1d964ad00827
jade243 <Aurelia.Autem@etu.unige.ch>
2019-04-17 21:39:07 +0200

    Normalize packages names

---
+++ classads-service/src/main/java/domain/service/AdServiceImpl.java
@@ -0,0 +1,72 @@
+package domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import domain.model.Ad;
+
+@ApplicationScoped
+@Transactional
+public class AdServiceImpl implements AdService{
+
+	@PersistenceContext(name="InmemoryPU")
+	private EntityManager em;
+
+	@Override
+	public boolean createAd(Ad ad) {
+		Optional<Ad> existing = this.getByTitle(ad.getTitle());
+		if(!existing.isPresent()) {
+			em.persist(ad);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<Ad> getAll() {
+		return em.createQuery("SELECT a FROM Ad a", Ad.class).getResultList();
+
+	}
+
+	@Override
+	public Optional<Ad> getByTitle(String title) {
+		List<Ad> ads = em.createQuery("SELECT a FROM Ad a WHERE LOWER(a.title) = LOWER('"+title+"')", Ad.class).getResultList();
+		if(ads.size() > 0) {
+			return Optional.of(ads.get(0));
+		}
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Ad> getById(long id) {
+		List<Ad> ads = em.createQuery("SELECT a FROM Ad a WHERE LOWER(a.id) = LOWER('"+id+"')", Ad.class).getResultList();
+		if(ads.size() > 0) {
+			return Optional.of(ads.get(0));
+		}
+		
+		return Optional.empty();
+	}
+
+	@Override
+	public void deleteAd(Ad ad) {
+		em.remove(em.contains(ad) ? ad : em.merge(ad));
+		
+	}
+
+	public EntityManager getEm() {
+		return em;
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+	
+	
+}
+
---
+++ user-service/src/main/java/api/UserEndpoint.java
@@ -0,0 +1,76 @@
+package api;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+
+import domain.model.User;
+import domain.service.UserService;
+
+@ApplicationScoped
+@Path("/User")
+public class UserEndpoint {
+	@Inject
+	private UserService userservice;
+	
+	public void setUserService(UserService us) {
+		userservice = us;
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAll() {
+		List<User> usl = userservice.getAll();
+		
+		Gson gson = new Gson();
+		return gson.toJson(usl);
+	}
+	
+	@POST
+	@Path("/new")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addNewUser(User u) {
+		if(userservice.createUser(u)) {
+			return "You inserted an ad ";
+		} else {
+			return "Error. This ad already exists";
+		}
+	}
+	
+	@DELETE
+	@Path("/delete")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUser(@QueryParam("id") String str_id) {
+		long id = Long.parseLong(str_id);
+		
+		Optional<User> popt = userservice.getById(id);
+		if (popt.isEmpty()) {
+			return "Error. There is no user with ID " + id;
+		} else {
+			User u = popt.get();
+			
+			try {
+				userservice.deleteUser(u);
+				return "Deleted classadd "+ u.toString();
+			} catch(IllegalArgumentException ex) {
+				System.err.println(ex.toString());
+				return "Some form of error occurred. Could not delete "+ u.toString();
+			}
+		}
+	}
+	
+}
