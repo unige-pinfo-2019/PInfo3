a25cef82baf58761e1d3b5b07895fdc04e719c17
LunarX <gibran.chevalley@etu.unige.ch>
2019-04-23 16:33:56 +0200

    added price to minimized ad

--- web-ui/src/components/MinimizedAd.vue
+++ web-ui/src/components/MinimizedAd.vue
@@ -9,10 +9,16 @@
       style="max-width: 20rem;"
       class="mb-2"
     >
-      <b-card-text>
+      <b-card-text id="descr">
         <!-- Some quick example text to build on the card title and make up the bulk of the card's content. -->
         {{ description }}
       </b-card-text>
+
+      <div class="price-wrapper">
+        <b-card-text id="price">
+          CHF {{ prix }}
+        </b-card-text>
+      </div>
 
       <!-- <b-button href="#" variant="outline-primary">Go somewhere</b-button> -->
     </b-card>
@@ -26,12 +32,33 @@ export default {
   props: {
     imgUrl: String, //https://picsum.photos/600/300/?image=25
     title: String,
-    description: String
+    description: String,
+    prix: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+#descr {
+  color: #666;
+}
 
+#price {
+  float: right;
+  margin-right: 10px;
+  font-weight: bold;
+  // font-size: 1.4em;
+}
+
+// #price-name {
+//   float: left;
+//   margin-bottom: 0px;
+//   color: #AAA;
+// }
+
+.price-wrapper {
+  margin-top: 30px;
+  font-size: 1.3em;
+}
 </style>
--- web-ui/src/views/AdsResult.vue
+++ web-ui/src/views/AdsResult.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="results">
-    <MiniAd title="Pc de collection à vendre" imgUrl="http://www.le-grenier-informatique.fr/medias/album/apple-iic-5.jpg" description="Je vend mon pc pour un bon prix, modèle Apple IIc je suis prêt à négocier le prix."/>
+    <MiniAd title="Pc de collection à vendre" prix="9500" imgUrl="http://www.le-grenier-informatique.fr/medias/album/apple-iic-5.jpg" description="Je vend mon pc pour un bon prix, modèle Apple IIc je suis prêt à négocier le prix."/>
   </div>
 </template>
 
