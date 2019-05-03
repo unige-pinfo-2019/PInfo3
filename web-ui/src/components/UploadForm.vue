b0ec5005e268a154d2ed7312942a126d3c01c874
David Alexander <david.alexander@etu.unige.ch>
2019-05-03 16:19:36 +0200

    Added upload to imgur at /upload

---
+++ web-ui/src/components/UploadForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="UploadForm">
+    <input type="file" @change="onFileChanged">
+    <br>
+    <button @click="onUpload">Upload!</button>
+    <br>
+    Lien de l'image :     {{ info }}
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'UploadForm',
+  data() {
+  return {
+    selectedFile: null,
+    info:null
+  }
+  },
+
+  methods: {
+    onFileChanged (event) {
+      this.selectedFile = event.target.files[0]
+      console.log(this.selectedFile)
+
+    },
+    onUpload() {
+      // Authentification
+      var config = {
+        headers: {'Authorization': 'Client-ID a98be453a893668'}
+      };
+      // Data
+      var data = new FormData();
+      data.append("image", this.selectedFile);
+
+      axios
+        .post('https://api.imgur.com/3/image',data, config)
+        .then(response => (this.info = response.data.data.link))
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
