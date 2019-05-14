2f61b3b124f349c106ff0bf2f893c0f6c0c8243a
Consolethinks <wewa@first-email.net>
2019-05-14 13:55:47 +0200

    readding presentation files + initial continuous deployment attempt

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 ###script to locally install docker files
-## classads-service
 ORIGINDIR=$(pwd)
 BASEDIR=$(dirname $(readlink -f "$0"))
+## classads-service -----------------------------------
 cd $BASEDIR/classads-service
 mvn package
 docker build --tag=2019pinfo3/classads-service:latest .
-## web-ui
+## web-ui ---------------------------------------------
 cd $BASEDIR/web-ui
 npm rebuild node-sass
 npm install dependencies
 npm run build
 docker build --tag=2019pinfo3/web-ui:latest .
-## api gateway
+## api gateway ----------------------------------------
 cd $BASEDIR/api-gateway
 docker build --tag=2019pinfo3/api-gateway:latest .
-## search-service
+## search-service -------------------------------------
 cd $BASEDIR/search-service
 mvn package
-docker build --tag=2019pinfo3/search-service:latest
+docker build --tag=2019pinfo3/search-service:latest .
+## user-service ---------------------------------------
+cd $BASEDIR/user-service
+mvn package
+docker build --tag=2019pinfo3/user-service:latest .
 
 
 ## ending
