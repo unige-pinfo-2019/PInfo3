59e8cc55fbf8951100570718ca699b28b391cdd0
David Alexander <david.alexander@etu.unige.ch>
2019-05-16 20:12:43 +0200

    Updated AdsResult

--- web-ui/src/views/AdsResult.vue
+++ web-ui/src/views/AdsResult.vue
@@ -4,12 +4,11 @@
     <div class="search-bar-wrapper">
       <Searchbar v-on:clicked="onClickSearch"/>
     </div>
-
     <div class="results-pub-container">
 
       <div class="results-wrapper">
         <template v-if="ads !=null">
-          <MiniAd class="mini-ad" v-for="ad in ads.data" :title="ad.title" :prix="ad.price" :id="ad.id" imgUrl="http://www.le-grenier-informatique.fr/medias/album/apple-iic-5.jpg" :description="ad.description" v-bind:key="ad.title"/>
+          <MiniAd class="mini-ad" v-for="ad in ads.data" :title="ad.title" :prix="ad.price" :id="ad.id" :imgUrl="ad.images" :description="ad.description" v-bind:key="ad.title"/>
 
           <MiniAd class="mini-ad" v-for="index in requiredEmpty" :key="index" title="" :prix=0 :id="0" imgUrl="" description="blank"/>
         </template>
