7bcd135491190124a96ccbd692f4c7e59af5943a
LunarX <gibran.chevalley@etu.unige.ch>
2019-05-26 19:55:10 +0200

    Improved minified ad design and readability

--- web-ui/src/components/MinimizedAd.vue
+++ web-ui/src/components/MinimizedAd.vue
@@ -3,7 +3,6 @@
     <div class="clicker" v-on:click="alert('Clicked')">
 
       <b-card v-if="title !== ''"
-      v-bind:title="shortenedTitle"
       v-bind:img-src="imgUrl"
       img-alt="Image"
       img-top
@@ -13,8 +12,18 @@
       >
 
       <a v-on:click="changePage(id)"></a>
+      <b-card-title>
+        <div class="title-container">
+          {{title}}
+        </div>
+      </b-card-title>
+
       <b-card-text id="descr">
-        {{ shortenedDescription }}
+        <!-- {{ description }} -->
+        <div class="descr-container">
+          {{description}}
+          <!-- Lorem ipsum dolor sit amet, consectetuer addksiojke kdoewpkdewiop  kdoewpk dopewk pdoewk dopw kpoedeipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus -->
+        </div>
       </b-card-text>
 
       <div class="price-wrapper">
@@ -50,8 +59,8 @@ export default {
     }
   },
   mounted() {
-    this.shortenTitle();
-    this.shortenDescription();
+    // this.shortenTitle();
+    // this.shortenDescription();
    },
   methods: {
     shortenTitle() {
@@ -63,6 +72,7 @@ export default {
       else {
         this.shortenedTitle = this.title;
       }
+      this.shortenedTitle = this.title;
       // console.log("Title length: " + this.title.length)
     },
     shortenDescription() {
@@ -74,6 +84,7 @@ export default {
       else {
         this.shortenedDescription = this.description;
       }
+      this.shortenedDescription = this.description;
       //console.log(this.description.length)
     },
     redirect() {
@@ -89,6 +100,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+
+/* mixin for multiline */
+@mixin multiLineEllipsis($lineHeight: 1.2em, $lineCount: 1, $bgColor: white){
+  overflow: hidden;
+  position: relative;
+  line-height: $lineHeight;
+  max-height: $lineHeight * $lineCount;
+  text-align: justify;
+  // margin-right: -1em;
+  padding-right: 1em;
+  &:before {
+    content: '...';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    width: 1em;
+    height: 1em;
+    margin-top: 0.2em;
+    background: $bgColor;
+  }
+}
 
 .card-img-top { // Forces the card image to be of the same size
     width: 100%;
@@ -109,14 +146,26 @@ a:hover {
 }
 
 .card-title {
-  height: 2.2em;
+  // height: 2.2em;
+  min-height: 2.2em;
+  @include multiLineEllipsis($lineHeight: 1.2em, $lineCount: 2, $bgColor: white);
+}
+
+.title-container {
+  font-size: 0.8em;
+  font-weight: bold;
 }
 
 #descr {
   color: #666;
   text-align: justify;
-  letter-spacing: -0.01em;
+  // letter-spacing: -0.01em;
   height: 120px;
+}
+
+.descr-container {
+  letter-spacing: -0.03em;
+  @include multiLineEllipsis($lineHeight: 1.2em, $lineCount: 7, $bgColor: white);
 }
 
 #price {
