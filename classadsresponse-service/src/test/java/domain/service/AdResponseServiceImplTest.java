fdee4f479c92cacf11a940c3d0c050e5fa127ae7
PavlosTserevelakis <pavlos.tserevelakis@etu.unige.ch>
2019-06-04 11:41:19 +0200

    new tests for classadsresponse

--- classadsresponse-service/src/test/java/domain/service/AdResponseServiceImplTest.java
+++ classadsresponse-service/src/test/java/domain/service/AdResponseServiceImplTest.java
@@ -14,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import domain.model.AdResponse;
@@ -94,6 +95,24 @@ class AdResponseServiceImplTest {
 		Assertions.assertEquals(respList, actual);
 		
 	}
+	
+	/*
+	@Test
+	public void getJsonListAdResponsesTest() {
+		AdResponse resp0 = new AdResponse(333, 17 , "Salut, il parait que tu vends une tondeuse", true);
+		AdResponse resp1 = new AdResponse(333, 17 , "Salut, oui", false);
+		AdResponse resp2 = new AdResponse(333, 17 , "Trop cool, tu me la fais à combien ?", true);
+		AdResponse resp3 = new AdResponse(333, 17 , "Désolé, elle n'est pas à vendre", false);
+		
+		List<AdResponse> responses = new ArrayList<> ();
+		responses.add(resp0);
+		responses.add(resp1);
+		responses.add(resp2);
+		responses.add(resp3);
+		
+		JsonArray jsa = ars.getJsonListAdResponses(responses);
+		
+	} */
 	
 	@Test
 	public void createAdFromJsonTest() {
